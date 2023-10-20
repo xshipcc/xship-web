9d4748de38a5d123f61fee27848f7e75a25b720f
weiaodi <1635654853@qq.com>
2023-10-20 09:58:07 +0800

    feat: 无人机,机场控制板调制

--- src/pages/dashboard/component/Awareness/center.tsx
+++ src/pages/dashboard/component/Awareness/center.tsx
@@ -2,7 +2,7 @@
  * @Author: weiaodi 1635654853@qq.com
  * @Date: 2023-09-14 08:59:17
  * @LastEditors: weiaodi 1635654853@qq.com
- * @LastEditTime: 2023-10-20 00:17:18
+ * @LastEditTime: 2023-10-20 09:51:43
  * @FilePath: \zero-admin-ui-master\src\pages\dashboard\component\Awareness\center.tsx
  * @Description:
  *
@@ -102,9 +102,13 @@ const AnalysisCenter: React.FC = (props) => {
                       <b></b>
                     </dd>
                   </dl>
+                  <div className={styles.powerLeft}>
+                    <div>电量</div>
+                    <div style={{ color: '#40e10f' }}>37%</div>
+                  </div>
                   <div className="round-1"></div>
                   <div className="round-2"></div>
-                  <div className="round-3">30%</div>
+                  <div className="round-3"></div>
                 </div>
               </Col>
               <Col span={12} className={styles.rightContent}>
@@ -147,9 +151,7 @@ const AnalysisCenter: React.FC = (props) => {
           <div className={styles.rightbox}>
             <div className={styles.box}>
               <Row>
-                <Col span={8} className={styles.leftContent}></Col>
-                {/*  */}
-                <Col span={16} className={styles.rightContent}>
+                <Col span={12} className={styles.leftContent}>
                   <Row gutter={[16, 24]} className={styles.buttonRow}>
                     <Col span={8}>
                       {/* @ts-ignore */}
@@ -178,6 +180,123 @@ const AnalysisCenter: React.FC = (props) => {
                       <AwarenessButton name={'回家'} over={'回家完成'} url={'/demo'} />
                     </Col>
                   </Row>
+                </Col>
+                {/*  */}
+                <Col span={12} className={styles.RightContent}>
+                  <div className="area-inbox-2">
+                    {/* 1.电池电压(数值)、2.电池温度(数值)、3.电池状态(数值)、4.风速M/s
+                    (数值)、5.风向(数值)、6.雨雪传感器
+                    (数值)、7.舱外温度C(数值)、8.舱外湿度%(数值)、9.舱内温度C数值
+                    )10.舱内湿度%(数值)、11.舱盖状态(文字 )、12.舱盖角度 (数值
+                    )、13.归位机构状态(文字)、14.充电机状态 (文字)、15.无人机状态(文字 ) */}
+                    <dl>
+                      <dd className="font-red ml-20">
+                        {/* <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            风速
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            31M/s <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            风向
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            310° <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            雨雪传感器
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            31% <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            充电机状态
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            31% <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            归位机构状态
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            31% <b></b>
+                          </Col>
+                        </Row> */}
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            电池电压
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            220v <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            电池温度
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            37.1133 <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            电池状态
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            37.1M/s <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            舱外温度
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            C <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            舱外湿度
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            40% <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            舱内温度
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            37.1C <b></b>
+                          </Col>
+                        </Row>
+                        <Row className="font-red ml-20">
+                          <Col span={6} style={{ color: 'white', fontFamily: 'YouSheBiaoTiHei' }}>
+                            舱内湿度
+                          </Col>
+                          <Col span={18} style={{ color: 'turquoise' }}>
+                            31% <b></b>
+                          </Col>
+                        </Row>
+                      </dd>
+                    </dl>
+                    <div className={styles.powerRight}>
+                      <div>电量</div>
+                      <div style={{ color: '#40e10f' }}>37%</div>
+                    </div>
+                    <div className="round-1-right"></div>
+                    <div className="round-2-right"></div>
+                    <div className="round-3-right"></div>
+                  </div>
                 </Col>
               </Row>
             </div>
